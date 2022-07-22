f2b6993c406a32926959dbddff57bf31404ca0f7
Orozpe <orozpe127@gmail.com>
2022-07-22 02:04:26 -0500

    Separador de section en css pt-1

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ btnSwitch.addEventListener('click', () => {
 	document.body.classList.toggle('dark');
 	btnSwitch.classList.toggle('active');
 	let element = document.getElementById("page-top");
+	element.classList.contains("dark .wave-cont::before")
+
 	if (element.classList.contains("dark")){
 		// alert("Activado");
 		// document.getElementById('dark_mode').style.visibility = "visible";
